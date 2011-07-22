d67f7bda22e4ca7caef639347d34640d125e27ec
Stefan Wieczorek <stefan.wieczorek@weemedia.de>
2011-07-22 02:25:22 +0200

    Change profiler sorting time desc

---
+++ htdocs/app/code/community/Wee/DeveloperToolbar/Block/Tab/Profiler.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Wee
+ * @package     Wee_DeveloperToolbar
+ * @author      Stefan Wieczorek <info@mgt-modules.com>
+ * @copyright   Copyright (c) 2011 (http://www.mgt-modules.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Wee_DeveloperToolbar_Block_Tab_Profiler extends Wee_DeveloperToolbar_Block_Tab
+{
+    protected $_profiler;
+    
+    public function __construct($name, $label)
+    {
+        parent::__construct($name, $label);
+        $this->setTemplate('wee_developertoolbar/tab/profiler.phtml');
+        $this->setIsActive(true);
+    }
+    
+    static public function compareTimers(array $timerA, array $timerB)
+    {
+       return ($timerA['sum'] < $timerB['sum']);
+    }
+    
+    public function getTimers($sorted = true)
+    {
+        $timers = Varien_Profiler::getTimers();
+        if (false === $sorted) {
+            return $timers;
+        } else {
+        	uasort($timers, array('self', 'compareTimers'));
+            return $timers;
+        }
+    }
+}
