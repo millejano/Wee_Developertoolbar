0613b7e55768ad6b3b8514ca033cd2da8c53324c
Stefan Wieczorek <stefan.wieczorek@weemedia.de>
2011-10-13 22:12:11 +0200

    .

--- htdocs/app/code/community/Wee/DeveloperToolbar/Helper/Data.php
+++ htdocs/app/code/community/Wee/DeveloperToolbar/Helper/Data.php
@@ -21,50 +21,22 @@
 
 class Wee_DeveloperToolbar_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public static function formatBytes($size, $praefix=true, $short= true) 
-    {
-        if ($praefix === true) {
-          if($short === true){
-            $norm = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 
-              'EB', 'ZB', 'YB');
-          } else {
-            $norm = array('Byte', 
-                          'Kilobyte', 
-                          'Megabyte', 
-                          'Gigabyte', 
-                          'Terabyte', 
-                          'Petabyte', 
-                          'Exabyte', 
-                          'Zettabyte', 
-                          'Yottabyte'
-                         );
-          }
-          $factor = 1000;
-        } else {
-          if($short === true) {
-            $norm = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 
-              'EiB', 'ZiB', 'YiB');
-          } else {
-            $norm = array('Byte', 
-              'Kibibyte', 
-              'Mebibyte', 
-              'Gibibyte', 
-              'Tebibyte', 
-              'Pebibyte', 
-              'Exbibyte', 
-              'Zebibyte', 
-              'Yobibyte'
-             );
-          }
-          $factor = 1024;
+    static public function formatBytes($bytes)  
+    { 
+        $size = $bytes / 1024; 
+        if ($size < 1024) { 
+            $size = number_format($size, 2); 
+            $size .= ' KB'; 
+        } else  { 
+            if ($size / 1024 < 1024)  { 
+                $size = number_format($size / 1024, 2); 
+                $size .= ' MB'; 
+            }  
+            else if ($size / 1024 / 1024 < 1024) { 
+                $size = number_format($size / 1024 / 1024, 2); 
+                $size .= ' GB'; 
+            }  
         } 
-        $count = count($norm) -1;
-        $x = 0;
-        while ($size >= $factor && $x < $count) { 
-          $size /= $factor; 
-          $x++;
-        } 
-        $size = sprintf("%01.2f", $size) . ' ' . $norm[$x];
         return $size; 
     }
     
